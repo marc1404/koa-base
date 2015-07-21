3e7555e178d4545c54b1fcac35204863a96d6dad
Marc <marc.vornetran@gmail.com>
2015-07-21 10:32:40 +0200

    Koala options

--- dotenv.js
+++ dotenv.js
@@ -2,4 +2,6 @@ var path = require('path');
 var dotenv = require('dotenv');
 var envPath = path.join(process.cwd(), '.env');
 
-dotenv.load({ path: envPath });
+module.exports = () => {
+    dotenv.load({ path: envPath });
+};
--- koala.js
+++ koala.js
@@ -1,12 +1,18 @@
-require('./lib/dotenv');
+require('dotenv')();
 
-var port = process.env.PORT;
+var _ = require('lodash');
+var defaults = {
+    port: process.env.PORT || 80,
+    serve: 'public'
+};
 
 exports.config = null;
 exports.run = null;
 
-exports.start = function(){
-    console.log('Node ' + process.version + ', port ' + port);
+exports.start = function start(options = {}){
+    options = _.defaultsDeep(options, defaults);
+
+    console.log('Node ' + process.version + ', port ' + options.port);
 
     var http = require('http');
     var koa = require('koa');
@@ -16,14 +22,14 @@ exports.start = function(){
     var app = koa();
 
     app.use(logger);
-    app.use(serve('public'));
+    app.use(serve(options.serve));
     app.use(bodyParser());
 
     if(exports.config){
         exports.config(app);
     }
 
-    var server = http.createServer(app.callback()).listen(port);
+    var server = http.createServer(app.callback()).listen(options.port);
 
     if(exports.run){
         exports.run(server);
